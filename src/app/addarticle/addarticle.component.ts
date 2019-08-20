de6c762ff64823a1bdf1291888db07ec56216fce
Ohm Patel <Ohm_Patel@epam.com>
2019-08-20 14:39:22 +0530

    validation Implemented

--- src/app/addarticle/addarticle.component.ts
+++ src/app/addarticle/addarticle.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Router} from '@angular/router';
 import { obj,ModelService } from '../model.service';
-import { FormGroup, FormBuilder,Validators} from '@angular/forms';
+import { FormGroup, FormBuilder,Validators, FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-addarticle',
@@ -24,12 +24,12 @@ export class AddarticleComponent implements OnInit {
   }
   createForm(){
     this.form = this.fb.group({
-          heading: [''],
-          description: [''],
-          content:[''],
-          date:[''],
-          author:[''],
-          sourceURL:['']
+          heading: new FormControl('',[Validators.required]),
+          description: new FormControl('',[Validators.required]),
+          content:new FormControl('',[Validators.required]),
+          date:new FormControl('',[Validators.required]),
+          author:new FormControl('',[Validators.required]),
+          sourceURL:new FormControl('',[Validators.required])
         });
   }
 
@@ -37,18 +37,10 @@ export class AddarticleComponent implements OnInit {
     this.createForm();
   }
   
-  saveClicked(){
-    if(this.validate(this.form.get("heading").value,this.form.get("description").value,this.form.get("content").value, this.form.get("author").value,this.form.get("date").value)){
-      this.newData.sourceText = this.form.get("heading").value;
-      this.newData.descriptionText = this.form.get("description").value;
-      this.newData.contentText = this.form.get("content").value;
-      this.newData.authorText = this.form.get("author").value;
-      this.newData.dateText = this.form.get("date").value;
-      this.newData.imageurlText = this.form.get("sourceURL").value;
-      this.model.addNews(this.newData);
-      alert("News Successfully Added");
-      this.route.navigateByUrl('');
-    }
+   saveClicked(){
+    
+    alert("News Successfully Added");
+    this.route.navigateByUrl('');
   }
   validate(heading,description,content,author,date){
    if(heading == "" || description == "" || content == "" || author == "" || date == ""){
