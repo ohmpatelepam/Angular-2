55ebed988cd1f0922908729b4a16ea45f0cf5fe9
ohmpatelepam <ohm_patel@epam.com>
2019-08-21 20:46:37 +0530

    temporary

--- src/app/addarticle/addarticle.component.ts
+++ src/app/addarticle/addarticle.component.ts
@@ -40,7 +40,7 @@ export class AddarticleComponent implements OnInit {
    saveClicked(){
     
     alert("News Successfully Added");
-    this.route.navigateByUrl('/home/bodycomponent/all');
+    this.route.navigateByUrl(window.localStorage.getItem("username") + '/home/bodycomponent/all');
   }
   validate(heading,description,content,author,date){
    if(heading == "" || description == "" || content == "" || author == "" || date == ""){
