4b7ae6c7926a334ed44863b9b9bd6f33bac5513a
Ohm Patel <Ohm_Patel@epam.com>
2019-08-22 10:58:31 +0530

    ng template and cutom news field added in dropdown

--- src/app/aside/aside.component.ts
+++ src/app/aside/aside.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { ModelService } from '../model.service';
 import { Router, RouterModule} from '@angular/router';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-aside',
   templateUrl: './aside.component.html',
@@ -10,13 +11,16 @@ import { Router, RouterModule} from '@angular/router';
 export class AsideComponent implements OnInit {
 
   public newsList:any;
-  
+  subscription: Subscription;
   constructor(private model:ModelService,private route:Router) { 
     this.newsList = undefined;
   }
 
   ngOnInit() {
     this.newsList = this.model.getData();
+    this.subscription =  this.model.getMessage().subscribe((value) => {
+      this.newsList = value;
+    })
   }
   changedNews(news){
     console.log(news);
